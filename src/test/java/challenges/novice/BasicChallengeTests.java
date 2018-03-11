08d727ee7fe309606f72f63eee30c75c93183d33
Brian Smith <crazycabo350@gmail.com>
2018-03-11 15:51:31 -0400

    Create basic challenge tests template class

---
+++ src/test/java/challenges/novice/BasicChallengeTests.java
@@ -0,0 +1,10 @@
+package challenges.novice;
+
+/**
+ * Created by Brian Smith on 3/11/18.
+ * Description: Use this template to complete the basic challenge tests exercise.
+ * Don't forget to assert on each method once per test.
+ */
+public class BasicChallengeTests {
+    
+}
