461e183ca0b55762515dd8027fc134cd903c24ea
Brian Smith <crazycabo350@gmail.com>
2018-03-10 23:11:21 -0400

    Create challenge class

---
+++ src/main/java/challenges/novice/BasicTestsChallenge.java
@@ -0,0 +1,31 @@
+package challenges.novice;
+
+/**
+ * Created by Brian Smith on 3/10/18.
+ * Description:
+ */
+public class BasicTestsChallenge {
+
+    public static boolean sumOf(int first, int second, int equals) {
+        return first + second == equals;
+    }
+
+    public static int returnSumOf(int first, int second) {
+        return first + second;
+    }
+
+    public static String joinString(String s, String withString) {
+        return s + withString;
+    }
+
+    public static String[] returnReversed(String[] s) {
+        int length = s.length;
+        String[] stringsReversed = new String[length];
+
+        for (int i = 0; i < length; i++) {
+            stringsReversed[i] = s[length - 1 - i];
+        }
+
+        return stringsReversed;
+    }
+}
