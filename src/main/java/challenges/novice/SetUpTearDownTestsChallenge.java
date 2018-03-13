47b7dd752b2bd1df4163f7346ab402350b0c1b33
Brian Smith <crazycabo350@gmail.com>
2018-03-12 20:18:34 -0400

    Create setup and teardown challenge and template classes

--- src/main/java/challenges/novice/SetUpTearDownTestsChallenge.java
+++ src/main/java/challenges/novice/SetUpTearDownTestsChallenge.java
@@ -1,8 +1,61 @@
 package challenges.novice;
 
+import java.util.Random;
+
 /**
  * Created by Brian Smith on 3/11/18.
- * Description:
+ * Project: junit5-jupiter-training
+ * Description: Use these methods with the setup and teardown challenges.
  */
 public class SetUpTearDownTestsChallenge {
+
+    private int[] assignedNumbers;
+    private int firstNumber;
+    private int secondNumber;
+    private int thirdNumber;
+
+    public SetUpTearDownTestsChallenge() {
+        this.assignedNumbers = new int[]{0, 0, 0};
+    }
+
+    public int getNumber(int number) {
+        switch (number) {
+            case 1:
+                firstNumber = getRandomNumber(1, 100);
+                return firstNumber;
+            case 2:
+                secondNumber = getRandomNumber(101, 1000);
+                return secondNumber;
+            case 3:
+                thirdNumber = getRandomNumber(1001, 10001);
+                return thirdNumber;
+            default:
+                return -1;
+        }
+    }
+
+    public void reportUsedValue(int number, int value) {
+        switch (number) {
+            case 1:
+                assignedNumbers[0] = value;
+                break;
+            case 2:
+                assignedNumbers[1] = value;
+                break;
+            case 3:
+                assignedNumbers[2] = value;
+                break;
+            default:
+                throw new Error("You did not specify a correct number, 1-3 only");
+        }
+    }
+
+    public boolean getCompletionStatus() {
+        return assignedNumbers == new int[]{firstNumber, secondNumber, thirdNumber};
+    }
+
+    private int getRandomNumber(int lowerBound, int upperBound) {
+        Random rand = new Random();
+        return rand.nextInt(upperBound) + lowerBound;
+    }
 }
---
+++ src/test/java/challenges/novice/SetUpTearDownChallengeTests.java
@@ -0,0 +1,21 @@
+package challenges.novice;
+
+/**
+ * Created by BRIAN.SMITH on 3/12/2018 at 12:10 PM
+ * Project: junit5-jupiter-training
+ * Description: Use this template to complete the setup / teardown challenge tests exercise.
+ * You will create both annotated class and test setup and teardown methods, along with 3 tests.
+ * See README file for more information.
+ */
+public class SetUpTearDownChallengeTests {
+
+    // SetUp Class
+
+    // TearDown Class
+
+    // SetUp Test
+
+    // TearDown Test
+
+    // Tests(3)
+}
